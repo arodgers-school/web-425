a370a5388e02d25fad9aa50f01c5b69a9e8bae0f
Adam School <adrodgers@my365.bellevue.edu>
2022-01-07 17:44:56 -0600

    Exercise 1.3

---
+++ week-1/hello-world/src/app/app.component.ts
@@ -0,0 +1,24 @@
+/*
+Title: 
+    Exercise 1.3
+Author: 
+    Adam Rodgers
+Date: 
+    01/07/2021
+Modified By: Adam Rodgers
+Description: Angular CLI
+Resources:
+    Bellevue University WEB425 Github Repo
+    Prof Krasso's YouTube
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  myWorld = "You are now in Adam's World!";
+}
