e52fc9fbba7d46001f2f0a0a160f2b70e3b82b5e
Adam School <adrodgers@my365.bellevue.edu>
2022-02-12 20:53:00 -0600

    WEB425 Week 6

--- week-6/gpa-calculator-app2/src/app/gpa/gpa.component.ts
+++ week-6/gpa-calculator-app2/src/app/gpa/gpa.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+/*
+Title: 
+    Assignment 6.4
+Author: 
+    Adam Rodgers
+Date: 
+    02/12/2022
+Modified By: Adam Rodgers
+Description: GPA Calculator App Pt 2
+Resources:
+    Bellevue University WEB425 Github Repo
+    Prof Krasso's YouTube
+*/
+
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-gpa',
   templateUrl: './gpa.component.html',
-  styleUrls: ['./gpa.component.css']
+  styleUrls: ['./gpa.component.css'],
 })
 export class GpaComponent implements OnInit {
+  @Input() gpaTotal: number;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- week-6/gpa-calculator-app2/src/app/home/home.component.ts
+++ week-6/gpa-calculator-app2/src/app/home/home.component.ts
@@ -37,6 +37,8 @@ export class HomeComponent implements OnInit {
     'F',
   ];
   transcriptEntries: Array<ITranscript> = [];
+  gpaTotal: number = 0;
+
   constructor() {
     this.transcriptEntry = {} as ITranscript;
   }
@@ -44,6 +46,57 @@ export class HomeComponent implements OnInit {
   saveEntry() {
     this.transcriptEntries.push(this.transcriptEntry);
     this.transcriptEntry = {} as ITranscript;
+  }
+
+  calculateResults() {
+    let gpa: number = 0;
+
+    for (let entry of this.transcriptEntries) {
+      switch (entry.grade) {
+        case 'A':
+          gpa += 4.0;
+          break;
+        case 'A-':
+          gpa += 3.7;
+          break;
+        case 'B+':
+          gpa += 3.33;
+          break;
+        case 'B':
+          gpa += 3.0;
+          break;
+        case 'B-':
+          gpa += 2.7;
+          break;
+        case 'C+':
+          gpa += 2.3;
+          break;
+        case 'C':
+          gpa += 2.0;
+          break;
+        case 'C-':
+          gpa += 1.7;
+          break;
+        case 'D+':
+          gpa += 1.3;
+          break;
+        case 'D':
+          gpa += 1.0;
+          break;
+        case 'D-':
+          gpa += 0.7;
+          break;
+        case 'F':
+          gpa += 0.0;
+          break;
+      }
+    }
+    this.gpaTotal = gpa / this.transcriptEntries.length;
+  }
+
+  clearEntries() {
+    this.transcriptEntries = [];
+    this.gpaTotal = 0;
   }
 
   ngOnInit(): void {}
