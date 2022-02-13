2b4fb9da0a6d6fa2d217130784a172612ede5134
Adam School <adrodgers@my365.bellevue.edu>
2022-02-12 18:49:18 -0600

    WEB425 Week 6

---
+++ week-6/gpa-calculator-app2/src/app/app.component.ts
@@ -0,0 +1,24 @@
+/*
+Title: 
+    Exercise 6.2
+Author: 
+    Adam Rodgers
+Date: 
+    02/12/2022
+Modified By: Adam Rodgers
+Description: Input/Output Properties, Pt 1
+Resources:
+    Bellevue University WEB425 Github Repo
+    Prof Krasso's YouTube
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'gpa-calculator-app1';
+}
